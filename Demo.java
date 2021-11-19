1b48b72c48b41202bf24e32c69dd023964ec0a67
nithiinnm <nithiinnm@gmail.com>
2021-11-19 14:20:01 +0530

    US-NithiCorrectChange-001

--- Demo.java
+++ Demo.java
@@ -1,7 +1,16 @@
 public class {
 	
+	Map<String,Object> mapObject = new HashMap<String,Object>();
 	public void gitCheckout(){
 		syso("Welcome to git")
 	}
+	
+	public void mergeConflictByNihti(){
+		
+		mapObject.put("Nithi",employee);
+		
+		syso(mapObject);
+	}
+	
 	
 }
